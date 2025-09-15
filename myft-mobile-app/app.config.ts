6f3f11543914e6fffefa25b875bc40f1d3c775e4
Jack Baum <jackbaum@umich.edu>
2025-09-15 10:47:51 -0400

    changed version

--- myft-mobile-app/app.config.ts
+++ myft-mobile-app/app.config.ts
@@ -11,7 +11,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     name: 'myft-mobile-app',
     slug: 'myft-mobile-app',
     scheme: 'myftmobileapp',
-    version: '1.0.4',
+    version: '1.0.5',
     orientation: 'portrait',
     icon: './assets/images/MYFT_APP_LOGO.png',
     userInterfaceStyle: 'automatic',
